6d839205cc3cb35ec30bb2540e86a1630aa8ee7f
Devansh <devanshkapoor9911@gmail.com>
2021-06-20 00:10:00 +0530

    adding try/catch

--- src/services/LoginValidate.js
+++ src/services/LoginValidate.js
@@ -6,30 +6,34 @@ class LoginValidate {
 
 
     async VerifyEmployee(employee) {
-        const res = await axios.post(EMPLOYEE_API_BASE_URL + '/api/v1/login', employee);
-        console.log("api response");
-        console.log(res);
-        if (res.status === 200){
-            if (res.data === "recep") {
-                window.location.replace(`${EMPLOYEE_API_BASE_URL}/${res.data}`)
+        try {
+            const res = await axios.post(EMPLOYEE_API_BASE_URL + '/api/v1/login', employee);
+            console.log("api response");
+            console.log(res);
+
+            if (res.status === 200) {
+                if (res.data === "recep") {
+                    window.location.replace(`${EMPLOYEE_API_BASE_URL}/${res.data}`)
+                }
+                else if (res.data === "waiter1") {
+                    window.location.replace(`${EMPLOYEE_API_BASE_URL}/${res.data}`)
+                }
+                else if (res.data === "waiter2") {
+                    window.location.replace(`${EMPLOYEE_API_BASE_URL}/${res.data}`)
+                }
+                else if (res.data === "chefdisplay") {
+                    window.location.replace(`${EMPLOYEE_API_BASE_URL}/${res.data}`)
+                }
+                else if (res.data === "chefdisplay1") {
+                    window.location.replace(`${EMPLOYEE_API_BASE_URL}/${res.data}`)
+                }
+                else {
+                    return res.data;
+                }
+
             }
-            else if (res.data === "waiter1") {
-                window.location.replace(`${EMPLOYEE_API_BASE_URL}/${res.data}`)
-            }
-            else if (res.data === "waiter2") {
-                window.location.replace(`${EMPLOYEE_API_BASE_URL}/${res.data}`)
-            }
-            else if (res.data === "chefdisplay") {
-                window.location.replace(`${EMPLOYEE_API_BASE_URL}/${res.data}`)
-            }
-            else if (res.data === "chefdisplay1") {
-                window.location.replace(`${EMPLOYEE_API_BASE_URL}/${res.data}`)
-            }
-            else{
-                return res.data;
-            }
-           
-        } else {
+        }
+        catch (e) {
             return 'Some Error in Api !!'
         }
     }
